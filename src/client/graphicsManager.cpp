d9e06436840cce0352ce9d68aead816221053c17
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-01-19 21:26:19 +0100

    quickfix player win

--- src/client/graphicsManager.cpp
+++ src/client/graphicsManager.cpp
@@ -196,21 +196,18 @@ void GraphicsManager::DrawBulletsGUI(const GameManager &gm,
 }
 
 void GraphicsManager::DrawWinnerText(const GameManager &gm) {
-  if (gm.winner_player_id) {
-    const char *text = TextFormat(
-        "%s PLAYER WIN", Constants::PLAYER_NAMES[gm.winner_player_id].c_str());
-    Vector2 text_length = MeasureTextEx(font, text, Constants::TEXT_WIN_SIZE,
-                                        Constants::TEXT_SPACING);
-    // DrawRectangle(0, 0, Constants::screenWidth, Constants::screenHeight,
-    //               Constants::BACKGROUND_COLOR_HALF_ALFA);
-    DrawTextPro(
-        win_font, text,
-        Vector2{(float)Constants::screenWidth / 2, (float)text_length.y / 2},
-        Vector2{text_length.x / 2, text_length.y / 2}, 0,
-        Constants::TEXT_WIN_SIZE, Constants::TEXT_SPACING,
-        Constants::PLAYER_COLORS[gm.winner_player_id]);
-    return;
-  }
+  const char *text = TextFormat(
+      "%s PLAYER WIN", Constants::PLAYER_NAMES[gm.winner_player_id].c_str());
+  Vector2 text_length = MeasureTextEx(font, text, Constants::TEXT_WIN_SIZE,
+                                      Constants::TEXT_SPACING);
+  // DrawRectangle(0, 0, Constants::screenWidth, Constants::screenHeight,
+  //               Constants::BACKGROUND_COLOR_HALF_ALFA);
+  DrawTextPro(
+      win_font, text,
+      Vector2{(float)Constants::screenWidth / 2, (float)text_length.y / 2},
+      Vector2{text_length.x / 2, text_length.y / 2}, 0,
+      Constants::TEXT_WIN_SIZE, Constants::TEXT_SPACING,
+      Constants::PLAYER_COLORS[gm.winner_player_id]);
 }
 
 void GraphicsManager::DrawRoomTitle() {
