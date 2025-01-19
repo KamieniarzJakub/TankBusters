b7291ab58c3b7faa49ba1357e3a299aeec1492de
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-01-19 22:04:44 +0100

    c

--- src/client/networking.cpp
+++ src/client/networking.cpp
@@ -429,6 +429,9 @@ void ClientNetworkManager::handle_network_event(uint32_t event) {
     if (!status) {
       return;
     }
+    gameManager().winner_player_id = UINT32_MAX;
+    flip_game_manager();
+    gameManager().winner_player_id = UINT32_MAX;
 
     if (player_id_that_left == player_id.load()) {
 
@@ -449,9 +452,6 @@ void ClientNetworkManager::handle_network_event(uint32_t event) {
       joinedRoom().players.at(player_id_that_left).state = PlayerInfo::NONE;
       flip_joined_room();
       joinedRoom().players.at(player_id_that_left).state = PlayerInfo::NONE;
-      gameManager().winner_player_id = UINT32_MAX;
-      flip_game_manager();
-      gameManager().winner_player_id = UINT32_MAX;
 
     } catch (const std::out_of_range &ex) {
     }
